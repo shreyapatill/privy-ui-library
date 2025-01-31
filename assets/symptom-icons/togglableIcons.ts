0e155ab7570fbf78c8ee55cf94c22db6e11d3c70
Shreya Patil <shreyap2917@gmail.com>
2025-01-31 18:42:50 -0500

    yellow dry skin icon complete

--- assets/symptom-icons/togglableIcons.ts
+++ assets/symptom-icons/togglableIcons.ts
@@ -25,6 +25,7 @@ import Vulvar from "@/assets/symptom-icons/symptom-reactions/vulvar";
 import LowerBack from "@/assets/symptom-icons/symptom-reactions/lower-back";
 import Headache from "@/assets/symptom-icons/symptom-reactions/headache";
 import NormalSkin from "@/assets/symptom-icons/symptom-reactions/normal-skin";
+import DrySkin from "@/assets/symptom-icons/symptom-reactions/dry-skin";
 
 // Decide the two possible "colorSet" options
 export type ColorSet = "blush" | "spring" | "fawn";
@@ -67,7 +68,7 @@ export const togglableIcons: ToggableIcon[] = [
   { id: "lower-back",   label: "Lower Back",    IconComponent: LowerBack,     colorSet: "fawn" },
   { id: "headache",     label: "Headache",      IconComponent: Headache,      colorSet: "fawn" },
   { id: "normal-skin",  label: "Normal",        IconComponent: NormalSkin,    colorSet: "fawn" },
-//  { id: "dry-skin",     label: "Dry",           IconComponent: DrySkin,       colorSet: "fawn" },
+  { id: "dry-skin",     label: "Dry",           IconComponent: DrySkin,       colorSet: "fawn" },
 //  { id: "oily-skin",    label: "Oily",          IconComponent: OilySkin,      colorSet: "fawn" },
 //  { id: "sensitive-skin",label: "Sensitive",    IconComponent: SensitiveSkin, colorSet: "fawn" },
 
